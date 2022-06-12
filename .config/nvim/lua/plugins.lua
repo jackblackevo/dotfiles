645058eab11ea7da7e231aa6b0c3b9089cac9f5d
ryan.c <jackblackevo@gmail.com>
2022-06-13 01:26:44 +0800

    Add Which Key for Neovim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -52,6 +52,13 @@ return require('packer').startup(function(use)
     end,
     cond = check_not_vscode
   }
+  use {
+    'folke/which-key.nvim',
+    config = function()
+      require('which-key').setup()
+    end,
+    cond = check_not_vscode
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
