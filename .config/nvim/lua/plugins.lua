52dcc9fa7013c378d3d965160a9004ca641ced9b
ryan.c <jackblackevo@gmail.com>
2022-06-16 03:03:47 +0800

    [nvim] add Intellij-like mapping for nvim-cmp

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -98,6 +98,18 @@ return require('packer').startup(function(use)
             ['<C-Space>'] = cmp.mapping.complete(),
             ['<C-e>'] = cmp.mapping.abort(),
             ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<Tab>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                local entry = cmp.get_selected_entry()
+                if not entry then
+                  cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                else
+                  cmp.confirm()
+                end
+              else
+                fallback()
+              end
+            end, { 'i', 's', 'c' })
           }),
           sources = cmp.config.sources({
             { name = 'nvim_lsp' },
