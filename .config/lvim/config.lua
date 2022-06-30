8e4be6ee06f4d3c481a1fc5fbdb74a9a0dca9a63
ryan.c <jackblackevo@gmail.com>
2022-06-30 12:47:37 +0800

    [lvim] enable for displays indentation in the first column of Indent Blankline

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -235,7 +235,7 @@ lvim.plugins = {
       vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
       vim.g.indent_blankline_buftype_exclude = { "terminal" }
       vim.g.indent_blankline_show_trailing_blankline_indent = false
-      vim.g.indent_blankline_show_first_indent_level = false
+      vim.g.indent_blankline_show_first_indent_level = true
       vim.g.indent_blankline_show_current_context = true
       vim.g.indent_blankline_show_current_context_start = true
     end
