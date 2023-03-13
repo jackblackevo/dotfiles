866f1d0c509fee573ebc79244d4095ff61c81ea9
ryan.c <jackblackevo@gmail.com>
2023-03-13 15:31:27 +0800

    [lvim] override `importModuleSpecifierPreference` for tsserver

--- Ubuntu/.config/lvim/config.lua
+++ Ubuntu/.config/lvim/config.lua
@@ -94,6 +94,14 @@ lvim.builtin.treesitter.rainbow.enable = true
 -- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
 -- require("lvim.lsp.manager").setup("pyright", opts)
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tsserver" })
+require("lvim.lsp.manager").setup("tsserver", {
+  init_options = {
+    preferences = {
+      importModuleSpecifierPreference = "relative"
+    }
+  }
+})
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
 -- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
