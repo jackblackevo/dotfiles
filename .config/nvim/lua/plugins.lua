7fc1a29bacc77c87f99f2d1d7c20b74f29c70752
ryan.c <jackblackevo@gmail.com>
2023-04-18 01:40:08 +0800

    [nvim] use `cmp_nvim_lsp.default_capabilities` instead of `cmp_nvim_lsp.update_capabilities`

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -155,7 +155,7 @@ return require('packer').startup(function(use)
         })
 
         local lspconfig = require('lspconfig')
-        local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+        local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
         lspconfig.tsserver.setup({
           capabilities = capabilities
