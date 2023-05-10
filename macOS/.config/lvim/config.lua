019164308b764cbfaa82f2cf843415d40c8a91d5
ryan.c <jackblackevo@gmail.com>
2023-05-10 15:02:42 +0800

    [lvim] update tabnine-nvim accept keymap

--- macOS/.config/lvim/config.lua
+++ macOS/.config/lvim/config.lua
@@ -204,7 +204,7 @@ lvim.plugins = {
     config = function()
       require("tabnine").setup({
         disable_auto_comment = true,
-        accept_keymap = "<Tab>",
+        accept_keymap = "<C-f>",
         dismiss_keymap = "<C-]>",
         debounce_ms = 800,
         suggestion_color = { gui = "#808080", cterm = 244 },
