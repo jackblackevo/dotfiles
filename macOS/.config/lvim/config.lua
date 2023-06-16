d6f7a3e5e7e8d953fd5ef9273e86eb759aee48d0
ryan.c <jackblackevo@gmail.com>
2023-06-16 16:52:47 +0800

    [lvim] add `BufferLineMoveNext`, `BufferLineMovePrev` keymappings

--- macOS/.config/lvim/config.lua
+++ macOS/.config/lvim/config.lua
@@ -29,6 +29,8 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<M-l>"] = ":BufferLineMoveNext<CR>"
+lvim.keys.normal_mode["<M-h>"] = ":BufferLineMovePrev<CR>"
 
 -- -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
