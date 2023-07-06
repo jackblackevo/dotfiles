92734be685460256a7d066b4d0d58204b2887b92
ryan.c <jackblackevo@gmail.com>
2023-07-06 16:39:52 +0800

    [nvim] use mini.surround instead of vim-surround

--- .config/vscode-neovim/lua/plugins.lua
+++ .config/vscode-neovim/lua/plugins.lua
@@ -17,21 +17,10 @@ require("lazy").setup({
     branch = 'master'
   },
   {
-    "tpope/vim-surround",
+    'echasnovski/mini.surround',
+    version = false,
     config = function()
-      vim.keymap.set('n', 'ds', '<Plug>Dsurround')
-      vim.keymap.set('n', 'cs', '<Plug>Csurround')
-      vim.keymap.set('n', 'cS', '<Plug>CSurround')
-      vim.keymap.set('n', 'ys', '<Plug>Ysurround')
-      vim.keymap.set('n', 'yS', '<Plug>YSurround')
-      vim.keymap.set('n', 'yss', '<Plug>Yssurround')
-      vim.keymap.set('n', 'ySs', '<Plug>YSsurround')
-      vim.keymap.set('n', 'ySS', '<Plug>YSsurround')
-      vim.keymap.set('x', 'gs', '<Plug>VSurround')
-      vim.keymap.set('x', 'gS', '<Plug>VgSurround')
-    end,
-    init = function()
-      vim.g.surround_no_mappings = 1
+      require('mini.surround').setup()
     end
   },
   {
