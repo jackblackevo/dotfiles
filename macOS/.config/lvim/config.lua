663b0db776cba042e9b9eccb6c7a782f6441d62c
ryan.c <jackblackevo@gmail.com>
2024-01-08 19:39:26 +0800

    [lvim] remove codota/tabnine-nvim

--- macOS/.config/lvim/config.lua
+++ macOS/.config/lvim/config.lua
@@ -252,21 +252,6 @@ lvim.plugins = {
     end
   },
   {
-    "codota/tabnine-nvim",
-    build = "./dl_binaries.sh",
-    config = function()
-      require('tabnine').setup({
-        disable_auto_comment = true,
-        accept_keymap = "<C-f>",
-        dismiss_keymap = "<C-]>",
-        debounce_ms = 800,
-        suggestion_color = { gui = "#808080", cterm = 244 },
-        exclude_filetypes = { "TelescopePrompt", "NvimTree" },
-        log_file_path = nil, -- absolute path to Tabnine log file
-      })
-    end
-  },
-  {
     "rmagatti/goto-preview",
     config = function()
       require('goto-preview').setup {
