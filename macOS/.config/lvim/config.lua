296de881dd15bdaa24874d953eebc25fd73ccb2a
ryan.c <jackblackevo@gmail.com>
2024-01-08 18:48:43 +0800

    [lvim] update tabnine-nvim activate config

--- macOS/.config/lvim/config.lua
+++ macOS/.config/lvim/config.lua
@@ -237,13 +237,14 @@ lvim.plugins = {
     "codota/tabnine-nvim",
     build = "./dl_binaries.sh",
     config = function()
-      require("tabnine").setup({
+      require('tabnine').setup({
         disable_auto_comment = true,
         accept_keymap = "<C-f>",
         dismiss_keymap = "<C-]>",
         debounce_ms = 800,
         suggestion_color = { gui = "#808080", cterm = 244 },
-        exclude_filetypes = { "TelescopePrompt" }
+        exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+        log_file_path = nil, -- absolute path to Tabnine log file
       })
     end
   },
