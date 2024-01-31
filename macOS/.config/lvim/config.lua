0893e69b16d5ad87cd40a530e05119f601ad61d8
ryan.c <jackblackevo@gmail.com>
2024-01-31 21:33:10 +0800

    [lvim] update mini.surround key mappings

--- macOS/.config/lvim/config.lua
+++ macOS/.config/lvim/config.lua
@@ -309,7 +309,17 @@ lvim.plugins = {
     'echasnovski/mini.surround',
     version = false,
     config = function()
-      require('mini.surround').setup()
+      require('mini.surround').setup({
+        mappings = {
+          add = '<c-s>a',            -- Add surrounding in Normal and Visual modes
+          delete = '<c-s>d',         -- Delete surrounding
+          find = '<c-s>f',           -- Find surrounding (to the right)
+          find_left = '<c-s>F',      -- Find surrounding (to the left)
+          highlight = '<c-s>h',      -- Highlight surrounding
+          replace = '<c-s>r',        -- Replace surrounding
+          update_n_lines = '<c-s>n', -- Update `n_lines`
+        }
+      })
     end
   },
   {
